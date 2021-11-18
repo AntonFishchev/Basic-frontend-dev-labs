3f3625da4aec23aa0b1982d8d4fbd263eeceb35f
Anton Fishchev <antonfishevv@gmail.com>
2021-11-18 04:47:20 +0300

    lab5s2

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,5 +1,28 @@
+new Swiper(".universities-slider", {
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+
+    watchOverflow: true,
+
+    slidesPerView: 1,
+    slidesPerGroup: 1,
+
+    breakpoints: {
+        800: {
+            slidesPerView: 2,
+            slidesPerGroup: 2,
+        },
+        1200: {
+            slidesPerView: 4,
+            slidesPerGroup: 4,
+        },
+    },
+});
+
 const iconMenu = document.querySelector(".menu__icon");
-const headerBtn = document.querySelector('.header__btn');
+const headerBtn = document.querySelector(".header__btn");
 
 if (iconMenu) {
     const bodyMenu = document.querySelector(".menu__body");
@@ -7,6 +30,6 @@ if (iconMenu) {
         document.body.classList.toggle("_lock");
         iconMenu.classList.toggle("menu__icon_active");
         bodyMenu.classList.toggle("menu__body_active");
-        headerBtn.classList.toggle("header__btn_active-menu")
+        headerBtn.classList.toggle("header__btn_active-menu");
     });
 }
