b8c38542f6bfd668e72fcc2bc0887443dd20bf9f
Anton Fishchev <antonfishevv@gmail.com>
2021-11-19 03:18:56 +0300

    lab5s3

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
-new Swiper(".universities-slider", {
+const universitiesSwiper = new Swiper(".universities-slider", {
     pagination: {
-        el: ".swiper-pagination",
+        el: ".universities-pagination",
         clickable: true,
     },
 
@@ -12,11 +12,91 @@ new Swiper(".universities-slider", {
     breakpoints: {
         800: {
             slidesPerView: 2,
-            slidesPerGroup: 2,
+            slidesPerGroup: 1,
         },
         1200: {
             slidesPerView: 4,
             slidesPerGroup: 4,
+        },
+    },
+});
+
+const countriesSwiper = new Swiper(".events__swiper", {
+    // navigation: {
+    //     nextEl: ".events-button-next",
+    //     prevEl: ".events-button-prev",
+    // },
+
+    pagination: {
+        el: ".events-pagination",
+        clickable: true,
+    },
+
+    watchOverflow: true,
+
+    slidesPerView: 1,
+    slidesPerGroup: 1,
+
+    breakpoints: {
+        1000: {
+            slidesPerView: 2,
+            slidesPerGroup: 1,
+        },
+    },
+});
+
+const eventsSwiper = new Swiper(".coutries__swiper", {
+    // navigation: {
+    //     nextEl: ".countries-button-next",
+    //     prevEl: ".countries-button-prev",
+    // },
+
+    pagination: {
+        el: ".countries-pagination",
+        clickable: true,
+    },
+
+    watchOverflow: true,
+    simulateTouch: true,
+    touchRatio: 1,
+
+    slidesPerView: 1,
+    slidesPerGroup: 1,
+
+    breakpoints: {
+        800: {
+            slidesPerView: 2,
+            slidesPerGroup: 1,
+        },
+        1200: {
+            slidesPerView: 3,
+            slidesPerGroup: 1,
+        },
+    },
+});
+
+const testimonialSwiper = new Swiper(".testimonial-swiper", {
+    // navigation: {
+    //     nextEl: ".countries-button-next",
+    //     prevEl: ".countries-button-prev",
+    // },
+
+    pagination: {
+        el: ".testimonial-pagination",
+        clickable: true,
+    },
+
+    watchOverflow: true,
+    simulateTouch: true,
+    touchRatio: 1,
+
+    slidesPerView: 1,
+    slidesPerGroup: 1,
+
+    breakpoints: {
+        800: {
+            slidesPerView: 2,
+            slidesPerGroup: 1,
         },
     },
 });
@@ -31,5 +111,12 @@ if (iconMenu) {
         iconMenu.classList.toggle("menu__icon_active");
         bodyMenu.classList.toggle("menu__body_active");
         headerBtn.classList.toggle("header__btn_active-menu");
+    });
+
+    window.addEventListener("scroll", function () {
+        document.body.classList.remove("_lock");
+        iconMenu.classList.remove("menu__icon_active");
+        bodyMenu.classList.remove("menu__body_active");
+        headerBtn.classList.remove("header__btn_active-menu");
     });
 }
